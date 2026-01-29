d76c03eea63b5a1451436c350e5e242d633ba60f
Jay <lyl.jay@qq.com>
2026-01-29 23:11:51 +0800

    ADD 用例管理接口

--- src/main/java/jay/chd123/problem/controller/CaseController.java
+++ src/main/java/jay/chd123/problem/controller/CaseController.java
@@ -1,7 +1,64 @@
 package jay.chd123.problem.controller;
 
+import jay.chd123.global.entity.Result;
+import jay.chd123.problem.entity.db.ProblemCase;
+import jay.chd123.problem.service.servieImpl.CaseServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/game/case")
 public class CaseController {
+    private final CaseServiceImpl caseService;
+
+    public CaseController(CaseServiceImpl caseService) {
+        this.caseService = caseService;
+    }
+    @PostMapping("/listByProblemId")
+    public Result<List<ProblemCase>> getCaseByProblemId(@RequestBody Map<String,Integer> req){
+        Integer problemId = req.get("problemId");
+        if(problemId == null){
+            return Result.error("参数错误");
+        }
+        List<ProblemCase> cases = caseService.getJudgesByPromptId(problemId);
+        return Result.success(cases);
+    }
+    @PostMapping("/create")
+    public Result<Object> createCase(@RequestBody ProblemCase problemCase){
+        if(problemCase.getSId() == null){
+            return Result.fail("SID不能为空");
+        }
+        problemCase.setType(ProblemCase.TYPE.JUDGE.name());
+        boolean success = caseService.save(problemCase);
+        if(success){
+            return Result.success(problemCase.getId());
+        }else {
+            return Result.error("创建失败");
+        }
+    }
+    @PostMapping("/update")
+    public Result<String> updateCase(@RequestBody ProblemCase problemCase){
+        Integer id = problemCase.getId();
+        if(id == null){
+            return Result.error("参数错误");
+        }
+        ProblemCase one = caseService.getById(problemCase.getId());
+        one.setInput(problemCase.getInput());
+        one.setOutput(problemCase.getOutput());
+        caseService.updateById(one);
+        return Result.success("更新成功");
+    }
+    @PostMapping("/delete")
+    public  Result<String> deleteCase(@RequestBody List<Integer> ids){
+        if(ids == null){
+            return Result.error("参数错误");
+        }
+        caseService.removeBatchByIds(ids);
+        return Result.success("删除成功");
+    }
 }
--- src/main/java/jay/chd123/problem/entity/db/ProblemCase.java
+++ src/main/java/jay/chd123/problem/entity/db/ProblemCase.java
@@ -3,6 +3,7 @@ package jay.chd123.problem.entity.db;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class ProblemCase {
     }
     @TableId(type = IdType.AUTO)
     private Integer id;
+    @JsonProperty("sId")
     private Integer sId;
     private String sCode;
     private String input;
